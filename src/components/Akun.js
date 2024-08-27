687f753ed7063298ee7dd1b7d438297d600de072
innoviewindotech <innoviewindotech@gmail.com>
2024-08-27 17:10:37 +0000

    yap

--- src/components/Akun.js
+++ src/components/Akun.js
@@ -3,22 +3,33 @@ import { useParams } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import { storage } from '../firebase/firebase';
 import { ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { ref as databaseRef, get } from 'firebase/database';
+import { ref as databaseRef, get, set, onValue } from 'firebase/database';
 import { getDatabase } from 'firebase/database';
+import ReactPlayer from 'react-player';
 import './Akun.css';
 
 const Akun = () => {
-  const { userId } = useParams(); // Get userId from the route parameters
+  const { userId } = useParams();
   const { user, setProfilePicUrl } = useContext(UserContext);
   const [userData, setUserData] = useState({});
   const [profilePicUrl, setProfilePicUrlState] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
   const [error, setError] = useState('');
+  const [showPortfolioForm, setShowPortfolioForm] = useState(false);
+  const [portfolioData, setPortfolioData] = useState({
+    coreProblem: '',
+    solutionDescription: '',
+    badges: '',
+    testimonials: '',
+    pitchingFee: '',
+    openMeetingDate: '',
+    youtubeTrailer: ''
+  });
+  const [submittedPortfolios, setSubmittedPortfolios] = useState([]);
 
   useEffect(() => {
     const database = getDatabase();
     if (userId) {
-      // Fetch user data based on userId
       const userRef = databaseRef(database, `users/${userId}`);
       get(userRef).then((snapshot) => {
         if (snapshot.exists()) {
@@ -31,7 +42,6 @@ const Akun = () => {
         setError('Failed to fetch user data');
       });
 
-      // Fetch profile picture
       const fetchProfilePic = async () => {
         try {
           const profilePicRef = storageRef(storage, `profilePictures/${userId}`);
@@ -43,6 +53,14 @@ const Akun = () => {
       };
 
       fetchProfilePic();
+
+      const portfolioRef = databaseRef(database, `portfolios/${userId}`);
+      onValue(portfolioRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const portfolios = Object.values(snapshot.val());
+          setSubmittedPortfolios(portfolios);
+        }
+      });
     }
   }, [userId]);
 
@@ -58,9 +76,9 @@ const Akun = () => {
       uploadBytes(profilePicRef, selectedFile)
         .then(() => getDownloadURL(profilePicRef))
         .then((url) => {
-          setProfilePicUrlState(url); // Update the profile picture URL
+          setProfilePicUrlState(url);
           if (user.uid === userId) {
-            setProfilePicUrl(url); // Also update the context if the current user is viewing their own profile
+            setProfilePicUrl(url);
           }
           alert('Foto profil berhasil diunggah!');
         })
@@ -70,6 +88,42 @@ const Akun = () => {
         });
     }
   };
+
+  const handlePortfolioFormChange = (e) => {
+    const { name, value } = e.target;
+    setPortfolioData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handlePortfolioSubmit = (e) => {
+    e.preventDefault();
+    const database = getDatabase();
+    const portfolioRef = databaseRef(database, `portfolios/${userId}`);
+    set(portfolioRef, portfolioData)
+      .then(() => {
+        alert('Portofolio Solusi berhasil disimpan!');
+        setShowPortfolioForm(false);
+        setSubmittedPortfolios([portfolioData, ...submittedPortfolios]);
+      })
+      .catch((error) => {
+        console.error('Error saving portfolio:', error);
+        alert('Gagal menyimpan portofolio solusi.');
+      });
+  };
+
+  const handleShareClick = () => {
+    const domain = "https://solusiku.vercel.app/"; // Ganti dengan domain yang digunakan
+    const profileLink = `${domain}/profile/${userId}`;
+    navigator.clipboard.writeText(profileLink)
+      .then(() => {
+        alert('Tautan profil berhasil disalin!'); // Menampilkan popup alert
+      })
+      .catch(() => {
+        alert('Gagal menyalin tautan profil.'); // Menampilkan popup alert jika terjadi kesalahan
+      });
+  };  
 
   return (
     <div className="akun-container">
@@ -102,9 +156,117 @@ const Akun = () => {
         <div className="info">
           <p><strong>Email:</strong> {userData.email || 'N/A'}</p>
           <p><strong>No Telpon:</strong> {userData.phoneNumber || 'N/A'}</p>
+          <div className="share-icon" onClick={handleShareClick}>
+            📤 Share Profile
+          </div>
         </div>
         {error && <p className="error-message">{error}</p>}
       </div>
+
+      {user && user.uid === userId && (
+        <>
+          <div className="separator-line"></div>
+          <div className="professional-dashboard">
+            <h3>Professional Dashboard</h3>
+            <button onClick={() => setShowPortfolioForm(!showPortfolioForm)}>
+              Portofolio Solusi
+            </button>
+
+            {showPortfolioForm && (
+              <form onSubmit={handlePortfolioSubmit} className="portfolio-form">
+                <label>
+                  Core Problem (Link YouTube):
+                  <input
+                    type="text"
+                    name="coreProblem"
+                    value={portfolioData.coreProblem}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  Masalah yang Diselesaikan:
+                  <textarea
+                    name="solutionDescription"
+                    value={portfolioData.solutionDescription}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  Badge dan Pencapaian:
+                  <textarea
+                    name="badges"
+                    value={portfolioData.badges}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  Testimoni Klien:
+                  <textarea
+                    name="testimonials"
+                    value={portfolioData.testimonials}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  Pitching Fee:
+                  <input
+                    type="text"
+                    name="pitchingFee"
+                    value={portfolioData.pitchingFee}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  Date Open Meeting:
+                  <input
+                    type="text"
+                    name="openMeetingDate"
+                    value={portfolioData.openMeetingDate}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <label>
+                  YouTube Trailer (Link):
+                  <input
+                    type="text"
+                    name="youtubeTrailer"
+                    value={portfolioData.youtubeTrailer}
+                    onChange={handlePortfolioFormChange}
+                  />
+                </label>
+                <button type="submit">Kirim</button>
+              </form>
+            )}
+          </div>
+        </>
+      )}
+
+      {submittedPortfolios.length > 0 && (
+        <div className="unix-929-portfolio-container">
+        <h3 className="unix-929-portfolio-title">Portofolio Solusi</h3>
+        {submittedPortfolios.reverse().map((portfolio, index) => (
+          <div key={index} className="unix-929-portfolio-card">
+            <h4>Portofolio #{index + 1}</h4>
+            {portfolio.coreProblem && (
+              <div className="unix-929-portfolio-video">
+                <ReactPlayer url={portfolio.coreProblem} controls={true} />
+              </div>
+            )}
+            <p className="unix-929-portfolio-text"><strong>Masalah yang Diselesaikan:</strong> {portfolio.solutionDescription}</p>
+            {portfolio.youtubeTrailer && (
+              <div className="unix-929-portfolio-video">
+                <ReactPlayer url={portfolio.youtubeTrailer} controls={true} />
+              </div>
+            )}
+            <p className="unix-929-portfolio-text"><strong>Badge dan Pencapaian:</strong> {portfolio.badges}</p>
+            <p className="unix-929-portfolio-text"><strong>Testimoni Klien:</strong> {portfolio.testimonials}</p>
+            <p className="unix-929-portfolio-text"><strong>Pitching Fee:</strong> {portfolio.pitchingFee}</p>
+            <p className="unix-929-portfolio-text"><strong>Date Open Meeting:</strong> {portfolio.openMeetingDate}</p>
+          </div>
+        ))}
+      </div>
+      
+      )}
     </div>
   );
 };
